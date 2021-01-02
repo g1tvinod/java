e9a173724c56aad664ccecce7f3b8e4dc30c0bb5
g1tvinod <45305248+g1tvinod@users.noreply.github.com>
2021-01-02 19:48:14 +0530

    Create Overloading.java

---
+++ method/Overloading.java
@@ -0,0 +1,33 @@
+public class Overloading {
+
+  public static int sum(int [] arr) {
+    int sum = 0;
+    for(int i : arr){
+    	sum = sum + i;
+    }
+    return sum;
+  }
+  //[1,2,3,4,5,6,7,8,9,10], 5
+  public static int sum(int [] arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < arr.length() || i < n ; i++){
+      sum = sum + a[i];
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    int [] arr1 = {1,2};
+    System.out.println("sum = "+sum(arr1));
+   
+    int [] arr2 = {1,2,3,4,5,6,7,8,9,10};
+    System.out.println(sum(arr2,5));
+    
+    int [] arr3 = {5,6,7};
+    System.out.println(sum(arr3), 10);
+    
+    int [] arr4 = {8,9};
+    System.out.println(sum(arr4));
+    
+  }
+}
